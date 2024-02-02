e1ded3a5018dedb1a49d2aa35e0a6d9a2fc22b49
Rutwik Kumbhar <kumbharrutwik@gmail.com>
2024-02-02 14:32:28 +0530

    project setup

---
+++ dilevaryboy/src/test/java/com/dilevaryboy/DilevaryboyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilevaryboy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DilevaryboyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
